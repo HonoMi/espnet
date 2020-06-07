cef1c00be9a36f8edf9dd9b01b570a44107c8438
honoka <magimagilmagi@gmail.com>
2020-06-07 18:32:02 +0900

    [update]

--- README.honoka.md
+++ README.honoka.md
@@ -7,6 +7,8 @@
     - 4.9.0以下はそもそも公式の要件を満たさない．
 * [SeanNaren/warp-ctc](https://github.com/SeanNaren/warp-ctc)のインストール
     - CMakeLists.txt中の` --std=c++14`を` --std=c++11`に変えてからビルドすること．
+* [FLAC-1.3.3](http://www.linuxfromscratch.org/blfs/view/svn/multimedia/flac.html)
+* [libsndfile-1.0.28](http://lfsbookja.osdn.jp/BLFS/svn-ja/multimedia/libsndfile.html)
 
 ## Entry Points
 * `./egs/librispeech/asr1/run.sh`
--- setup.sh
+++ setup.sh
@@ -5,9 +5,14 @@ CUDAROOT=/usr/local/cuda/
 module load gcc/7.4.0
 # module load gcc/4.8.5
 
-export PATH=${HOME}/.local/gcc-4.9.2/bin:${PATH}
-export INCLUDE=${HOME}/.local/gcc-4.9.2/include:${INCLUDE}
-export LD_LIBRARY_PATH=${HOME}/.local/gcc-4.9.2/lib64:${HOME}/.local/gcc-4.9.2/lib:${LD_LIBRARY_PATH}
+export PATH=`pwd -r`/contrib/bin:${PATH}
+export INCLUDE=`pwd -r`/contrib/include:${INCLUDE}
+export LD_LIBRARY_PATH=`pwd -r`/contrib/lib64:`pwd -r`/contrib/lib:${LD_LIBRARY_PATH}
+
+
+# export PATH=${HOME}/.local/gcc-4.9.2/bin:${PATH}
+# export INCLUDE=${HOME}/.local/gcc-4.9.2/include:${INCLUDE}
+# export LD_LIBRARY_PATH=${HOME}/.local/gcc-4.9.2/lib64:${HOME}/.local/gcc-4.9.2/lib:${LD_LIBRARY_PATH}
 
 module load cuda/10.2/10.2.89
 module load nccl/2.6/2.6.4-1
